cedce5f264ddeffb3a481a8d05ce4f775cf95749
Vinicius Anjos <anjos.vinicius@hotmail.com>
2023-05-11 17:38:02 -0500

    Primeiro teste de API

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fnt"
+	"log"
+	"net/http"
+)
+
+func main() {
+    http.HandleFunc("/users", getUsers)
+	fnt.Println("api is on :8080")
+
+	log.Fatal(http.ListenenAndServe(":8080", nil))
+}
+
+type Users struct {
+	Id int `json:"id"`
+	Name string `json:"name"`
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content=Type", "application/json")
+	json.NewEncoder(w).Encode([]User{
+		Id: 1,
+		Name: "Vinicius",
+	})
+}
