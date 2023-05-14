09088aa2e0c4b84660eb5ff62cb301138515b9b8
asvinicius <you@example.com>
2023-05-14 12:40:41 -0500

    Initial Version Hello World

--- main.go
+++ main.go
@@ -1,27 +1,9 @@
 package main
 
 import (
-	"fnt"
-	"log"
-	"net/http"
+	"fmt"
 )
 
 func main() {
-    http.HandleFunc("/users", getUsers)
-	fnt.Println("api is on :8080")
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-type Users struct {
-	Id int `json:"id"`
-	Name string `json:"name"`
-}
-
-func getUsers(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content=Type", "application/json")
-	json.NewEncoder(w).Encode([]User{
-		Id: 1,
-		Name: "Vinicius",
-	})
+	fmt.Println("Olá Mundo!")
 }
