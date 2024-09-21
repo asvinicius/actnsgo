8f25bfb96a5ff494f5ff65cdd8520f6c1e5b1726
Vinicius Santos <anjos.vinicius@hotmail.com>
2024-09-20 23:37:50 -0500

    Insert super NOT OK

--- super/entity.go
+++ super/entity.go
@@ -3,7 +3,7 @@ package super
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Super struct {
-	ID       primitive.ObjectID `json:"id,omitempty" bson:"id,omitempty"`
+	ID       primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 	Name     string             `json:"name" bson:"name"`
 	Login    string             `json:"login" bson:"login"`
 	Password string             `json:"password" bson:"password"`
